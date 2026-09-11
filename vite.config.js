c6321aac93091713f064052a3e1b0a02702a6838
Leviatank <furgyvale@live.it>
2026-09-11 21:33:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/techvalen/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-  publicDir: 'public', // ✅ aggiungi questa riga
+  publicDir: 'public',
   build: {
-    outDir: 'dist', // ✅ assicura che la build finisca in dist
+    outDir: 'dist',
   },
 });
